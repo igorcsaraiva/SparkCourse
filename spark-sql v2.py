a40c078b6fb55d88487dcba6da68f12328b7acd4
igorcsaraiva <igorcsaraiva@hotmail.com>
2022-06-27 21:40:30 -0300

    spark sql usando funcoes

---
+++ spark-sql v2.py
@@ -0,0 +1,10 @@
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder.appName('Spark-Sql').getOrCreate()
+
+data = spark.read.option("header","true").option("inferSchema","true").csv('/home/igor/Documentos/SparkCourse/fakefriends.csv')
+
+data.printSchema()
+
+data.groupBy("age").avg("friends").orderBy("avg(friends)").show(500)
+
