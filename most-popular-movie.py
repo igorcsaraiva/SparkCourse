d57e4fe435186c9a29d1242cc5b084310e8fd20d
igorcsaraiva <igorcsaraiva@hotmail.com>
2022-07-10 14:54:59 -0300

    implementado nome do filme

--- most-popular-movie.py
+++ most-popular-movie.py
@@ -1,4 +1,5 @@
 from pyspark.sql import SparkSession
+from pyspark.sql import functions as func
 from pyspark.sql.types import StructType,StructField,IntegerType,FloatType 
 
 
@@ -16,3 +17,20 @@ df_table.registerTempTable("filmes")
 df = spark.sql("Select IdFilme, count(IdFilme) as MaisVotado from filmes where Estrelas = 5 group by IdFilme order by 2 desc")
 
 df.show()
+
+
+spark_item = spark.sparkContext.textFile('/home/igor/Documentos/SparkCourse/ml-100k/u.item')
+
+spark_item = spark_item.map(lambda x: (x.split('|')[0],x.split('|')[1])).collect()
+
+name_films = spark.sparkContext.broadcast(spark_item)
+
+def lookup_name(id):
+    return name_films.value[id - 1][1]
+
+lookup_name_udf = func.udf(lookup_name)
+
+df = df.withColumn('NomeFilme', lookup_name_udf(func.col('IdFilme')))
+
+df.show()
+
